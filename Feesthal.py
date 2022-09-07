e203f0a980bc22582b4d5c60e10a7f622be9ba88
Tim <tim@MacBook-Air-van-Tim.local>
2022-09-07 09:32:10 +0200

    de feesthal en feestlunch

---
+++ Feesthal.py
@@ -0,0 +1,12 @@
+from importlib import import_module
+import math 
+ppl = 4
+toegangsticket = 7.45*ppl
+a = 5
+tijd = 45
+prijs = tijd/a
+Vrbril = 0.37*prijs
+totaal = Vrbril+toegangsticket * ppl
+print("Dit geweldige dagje-uit met" ,ppl,"mensen in de Speelhal met" ,tijd, "minuten VR kost je maar" ,totaal, "euro!")
+
+
--- Feestlunch.py
+++ Feestlunch.py
@@ -1,8 +1,11 @@
 from importlib import import_module
 import math
-croissant = 0.39*17
-stokbrood = 2.78*2
-kortingsbon = 0.50*3
-
-print (croissant+stokbrood-kortingsbon)
-
+Acroissant = 17
+croissant = 0.39*Acroissant
+Astok = 2
+stokbrood = 2.78*Astok
+Akorting = 3
+Korting = 0.50*Akorting
+Eind = croissant+stokbrood-Korting
+print(croissant+stokbrood-Korting)
+print("De feestlunch kost je bij de bakker" ,Eind, "euro voor de" ,Acroissant, "croissantjes en de",Astok, "stokbroden als de",Akorting, "kortingsbonnen nog geldig zijn!")
