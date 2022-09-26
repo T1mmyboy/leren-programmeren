129f541682a34cbc01f9e9761e198ace9c05057d
Tim <tim@MacBook-Air-van-Tim.local>
2022-09-26 12:55:35 +0200

    Opdracht 2 t/m 4

---
+++ Opdracht2.py
@@ -0,0 +1,43 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 2')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.drop()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.drop()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.moveLeft()
+robotArm.drop()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.moveRight()
+robotArm.drop()
+
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
---
+++ robotarm-python-2021-main/Opdracht3.py
@@ -0,0 +1,33 @@
+from timeit import repeat
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 3')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.drop()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.drop()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.drop()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+robotArm.drop()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+robotArm.moveLeft()
+
+
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
